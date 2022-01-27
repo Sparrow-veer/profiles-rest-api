d97be98111ef8d2873c21da0085b48057b3ba1df
prudhvi <k.prudhvireddy1996@gmail.com>
2022-01-27 04:29:36 +0000

    fixed errors and created the admin

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -22,7 +22,7 @@ class UserProfileManager(BaseUserManager):
         return user
 
 
-    def create_super_user(self,email,name,password):
+    def create_superuser(self,email,name,password):
         user=self.create_user(email,name,password)
 
         user.is_superuser=True
@@ -44,7 +44,7 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     objects=UserProfileManager()
 
     USERNAME_FIELD='email'
-    REQUIRED_FIELD=['name']
+    REQUIRED_FIELDS=['name']
 
     def get_full_name(self):
         """retrieve full name"""
