98c65ec13b5cf9b66a861b5c9c6afe8d66c2d1be
prudhvireddy <k.prudhvireddy1996@gmail.com>
2022-01-27 16:34:21 +0530

    added permissions and authentication

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,9 +4,14 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 
+from rest_framework.authentication import TokenAuthentication
 
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
+
+
 # Create your views here.
 
 class HelloApiView(APIView):
@@ -92,3 +97,12 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self,request,pk=None):
         """handle removing a object"""
         return Response({'http_method':'DELETE'})
+
+
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class=serializers.UserProfileSerializer
+    queryset=models.UserProfile.objects.all()
+    authentication_classes=(TokenAuthentication,)
+    Permission_classes=(permissions.UpdateOwnProfile,)
