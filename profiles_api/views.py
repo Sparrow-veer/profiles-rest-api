7194c412f80ec7cbc88a37e10225320f25c13caf
prudhvireddy <k.prudhvireddy1996@gmail.com>
2022-02-01 13:21:47 +0530

    added login feauture

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -10,7 +10,8 @@ from rest_framework import filters
 from profiles_api import serializers
 from profiles_api import models
 from profiles_api import permissions
-
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 # Create your views here.
 
@@ -108,3 +109,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     Permission_classes=(permissions.UpdateOwnProfile,)
     filter_backends=(filters.SearchFilter,)
     search_fields=('name','email',)
+
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes=api_settings.DEFAULT_RENDERER_CLASSES
